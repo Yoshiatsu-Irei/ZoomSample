58a54b32511f848cdc0fbb3f00a22c5dd45fe57b
Yoshiatsu-Irei <yoshiatsu.irei@fuller.co.jp>
2021-04-28 11:40:34 +0900

    install pod

---
+++ CameraApp/CameraApp/ImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePicker.swift
+//  CameraApp
+//
+//  Created by Yoshiatsu Irei on 2021/04/27.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePicker()
+    }
+}
